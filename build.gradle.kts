44b82db0862a550ed82842a0b1df6484fabcc9f2
Efeturi Money <efeturi.money@gmail.com>
2021-01-08 04:49:19 +0100

    Add a task to generate the local repo used for reproducing this.
    Saves having to create a new repo.

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,8 +4,12 @@ buildscript {
     jcenter()
   }
   dependencies {
+    // import/sync worked previously using AGP 4.1.1, alternate commenting each versions to check
+    // classpath("com.android.tools.build:gradle:4.1.1")
     classpath("com.android.tools.build:gradle:4.2.0-beta03")
+
     classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.21")
+    classpath("org.eclipse.jgit:org.eclipse.jgit:5.10.0.202012080955-r")
   }
 }
 
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,8 +5,8 @@ sourceControl {
   gitRepository(file("./repo-ui-tests").toURI()) {
     producesModule("repro.sample.internal:ui-tests")
     plugins {
-      // this is gotten from the included build "build-ui-tests"  and is needed to
-      // define build logic for a dependency that isnt normally built with gradle
+      // this plugin is defined in the included build "build-ui-tests" and contains logic to
+      // define a build for the git repository, which doesn't contain build logic itself.
       id("repro-ui-tests")
     }
   }
